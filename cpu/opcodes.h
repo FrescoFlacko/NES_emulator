103f186fe728a25a20527f2adc6017599903a80f
Yanique Andre <yanique.andre@gmail.com>
2018-09-08 20:33:27 -0400

    Added two opcodes - ANC and AND

--- cpu/opcodes.h
+++ cpu/opcodes.h
@@ -1,1 +1,9 @@
 #include "cpu.h"
+
+#define ANC (value) ({ accumulator &= value; \
+  setflag(n, accumulator & highbit(accumulator)); \
+  setflag(c, accumulator & highbit(accumulator)); })
+  
+#define AND (value) ({ accumulator &= value; \
+  setflag(n, accumulator & highbit(accumulator)); \
+  setflag(z, !accumulator); })
